d2d1a93a8b0caacd8a0c97919afbe500f3592b7d
Paolo Bueno <paolohaji@gmail.com>
2016-11-14 12:57:32 -0200

    Implement photo

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Photo from './Photo';
 import { Link } from 'react-router';
 export default class PhotoGrid extends React.Component {
   render() {
-    return <div className="single-photo">
-      I'm the photo grid
-    </div>
+    return <div className="photo-grid">
+    {this.props.posts.map((post, i) => <Photo key={post.code} i={i} post={post} {...this.props} />)}
+   </div>
   }
 }
