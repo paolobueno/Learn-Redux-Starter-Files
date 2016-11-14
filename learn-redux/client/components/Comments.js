45324d2cbc0dc11dca131559774f1151c8e31cef
Paolo Bueno <paolohaji@gmail.com>
2016-11-14 13:15:39 -0200

    Add comments and update single

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Comments extends React.Component {
+  render() {
+    return <div className="comment">
+      I'm the comments
+    </div>
+  }
+}
--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
+
 export default class Single extends React.Component {
   render() {
+    const i = this.props.posts.findIndex(
+      (p) => p.code === this.props.params.postId);
+    const post = this.props.posts[i];
     return <div className="single-photo">
-      I'm the single photo
+      <Photo i={i} post={post} {...this.props} />
+      <Comments/>
     </div>
   }
 }
