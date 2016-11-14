32e892d0c2a7cf1572cbaf17067dfdea1850b62c
Paolo Bueno <paolohaji@gmail.com>
2016-11-14 12:00:26 -0200

    Add reducers

---
+++ learn-redux/client/reducers/comments.js
@@ -0,0 +1,9 @@
+// a reducer takes in two things:
+
+// 1. the action (info about what happened)
+// 2. copy of the state
+
+export default function comments(state = [], action) {
+  console.log(state, action);
+  return state;
+}
